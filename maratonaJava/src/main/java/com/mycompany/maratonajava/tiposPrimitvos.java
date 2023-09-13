eb565fad392c401b9a9e0fc18f5d5f42431d618b
cesarblessed <cesardepaula80@gmail.com>
2023-09-13 14:51:22 -0300

    tiposPrimitivos2 commit

--- maratonaJava/src/main/java/com/mycompany/maratonajava/tiposPrimitvos.java
+++ maratonaJava/src/main/java/com/mycompany/maratonajava/tiposPrimitvos.java
@@ -3,6 +3,10 @@ package com.mycompany.maratonajava;
 public class tiposPrimitvos {
     public static void main(String[] args){
         int idade = 18;
+        char letra = 'a';
+     
         System.out.println(idade);
+        System.out.println(letra);
+        
     }
 }
