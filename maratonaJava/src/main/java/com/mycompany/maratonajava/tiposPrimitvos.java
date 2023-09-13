957da3abd856893208caf56e5522c7732c0e76df
cesarblessed <cesardepaula80@gmail.com>
2023-09-13 14:54:52 -0300

    tiposPrimitivos5

--- maratonaJava/src/main/java/com/mycompany/maratonajava/tiposPrimitvos.java
+++ maratonaJava/src/main/java/com/mycompany/maratonajava/tiposPrimitvos.java
@@ -5,10 +5,12 @@ public class tiposPrimitvos {
         int idade = 18;
         char letra = 'a';
         double numero = 3.5;
+        float valor = 7;
         boolean tipo = false;
         System.out.println(idade);
         System.out.println(letra);
         System.out.println(numero);
         System.out.println(tipo);
+        System.out.println(valor);
     }
 }
