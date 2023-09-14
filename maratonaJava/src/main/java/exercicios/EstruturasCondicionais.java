e9ee07fa1f2aca75861fd15b416f1807031e8104
cesarblessed <cesardepaula80@gmail.com>
2023-09-14 14:31:15 -0300

    estruturas condicionais 3

--- maratonaJava/src/main/java/exercicios/EstruturasCondicionais.java
+++ maratonaJava/src/main/java/exercicios/EstruturasCondicionais.java
@@ -4,8 +4,22 @@ public class EstruturasCondicionais {
     public static void main(String[] args) {
        int idade = 18;
        
+       
        if(idade >= 18){
            System.out.println("Maior de idade");
+       }
+       
+       if(idade <=12){
+           System.out.println("Criança");
+       }else if(idade >12 && idade <=18){
+           System.out.println("Adolescente");
+       }else{
+           System.out.println("Adulto");
+       }
+       
+       switch(idade){
+           case 18:
+               System.out.println("Maior de idade");
        }
     }
 }
