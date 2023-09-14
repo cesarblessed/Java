8f0ccbd73b0308dfa8af8e29d0aad8fba35f42fe
cesarblessed <cesardepaula80@gmail.com>
2023-09-14 13:44:13 -0300

    commit na branch main

---
+++ maratonaJava/src/main/java/com/mycompany/maratonajava/MaratonaJava.java
@@ -0,0 +1,7 @@
+package com.mycompany.maratonajava;
+
+public class MaratonaJava {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ maratonaJava/src/main/java/com/mycompany/maratonajava/tiposPrimitvos.java
@@ -0,0 +1,16 @@
+package com.mycompany.maratonajava;
+
+public class tiposPrimitvos {
+    public static void main(String[] args){
+        int idade = 18;
+        char letra = 'a';
+        double numero = 3.5;
+        float valor = 7;
+        boolean tipo = false;
+        System.out.println(idade);
+        System.out.println(letra);
+        System.out.println(numero);
+        System.out.println(tipo);
+        System.out.println(valor);
+    }
+}
