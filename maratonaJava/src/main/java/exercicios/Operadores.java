6b0c6d5c68810eb0a9a18973043c7b687c341eb7
cesarblessed <cesardepaula80@gmail.com>
2023-09-14 14:19:55 -0300

    operadores parte 2 commit

--- maratonaJava/src/main/java/exercicios/Operadores.java
+++ maratonaJava/src/main/java/exercicios/Operadores.java
@@ -4,12 +4,16 @@ public class Operadores {
     public static void main(String[] args) {
        int num1 = 15;
        int num2 = 10;
+       boolean igual = 10 == 10;
+       
        
         System.out.println(num1+num2);
         System.out.println(num1*num2);
         System.out.println(num1/num2);
         System.out.println(num1-num2); 
-        System.out.println(num1%num2); 
+        System.out.println(num1%num2);
+        System.out.println(igual); 
+        System.out.println(num1>num2); //retorna um boolean
     }
     
 }
