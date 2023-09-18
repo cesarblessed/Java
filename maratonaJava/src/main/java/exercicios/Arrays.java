ad11c617834d58616bda1f319bf98d9611b0ebd1
cesarblessed <cesardepaula80@gmail.com>
2023-09-18 11:03:44 -0300

    arrays commit

---
+++ maratonaJava/src/main/java/exercicios/Arrays.java
@@ -0,0 +1,13 @@
+package exercicios;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+        idades[0] = 20;
+        idades[1] = 10;
+        idades[2] = 25;
+        
+        System.out.println(idades[0]);
+    }
+    
+}
