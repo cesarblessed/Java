dd08822fb3b3dfba5504738418f16108956e54b3
cesarblessed <cesardepaula80@gmail.com>
2023-09-19 08:57:45 -0300

    classes 2 commit

--- maratonaJava/src/main/java/myClasses/PessoaTeste.java
+++ maratonaJava/src/main/java/myClasses/PessoaTeste.java
@@ -3,5 +3,12 @@ package myClasses;
 public class PessoaTeste {
     public static void main(String[] args) {
         Pessoa pessoa = new Pessoa();
+        pessoa.nome = "cesar";
+        pessoa.idade = 20;
+        pessoa.salario = 5.500;
+        
+        System.out.println(pessoa.nome);
+        System.out.println(pessoa.idade);
+        System.out.println(pessoa.salario);
     }
 }
