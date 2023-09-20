70a3082db6b1d366e046086ee5832023133deabc
cesarblessed <cesardepaula80@gmail.com>
2023-09-20 11:20:15 -0300

    metodos 1 commit

---
+++ maratonaJava/src/main/java/metodos/Somar.java
@@ -0,0 +1,8 @@
+package metodos;
+
+public class Somar {
+    
+    public void somarDoisNumeros(){
+        System.out.println(10 + 10);
+    }
+}
---
+++ maratonaJava/src/main/java/metodos/SomarNumeros.java
@@ -0,0 +1,9 @@
+package metodos;
+
+public class SomarNumeros {
+    public static void main(String[] args) {
+        Somar somar = new Somar();
+        somar.somarDoisNumeros();
+    }
+    
+}
