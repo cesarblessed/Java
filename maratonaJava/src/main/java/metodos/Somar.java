875b89722936197f511a5c03fc4182b500a4e24d
cesarblessed <cesardepaula80@gmail.com>
2023-09-20 11:23:48 -0300

    metodos 2 commit

--- maratonaJava/src/main/java/metodos/Somar.java
+++ maratonaJava/src/main/java/metodos/Somar.java
@@ -1,8 +1,12 @@
 package metodos;
 
 public class Somar {
-    
     public void somarDoisNumeros(){
         System.out.println(10 + 10);
+        
+        int num1 = 15;
+        int num2 = 10;
+        
+        System.out.println(num1+num2);
     }
 }
